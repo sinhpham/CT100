24366751a3ab12256de0d8b40495f3107c427077
Sinh Pham <phamansinh@gmail.com>
2014-08-06 14:37:14 -0600

    HomeVM.

---
+++ CT100/ViewModels/HomeVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace CT100
+{
+    public class HomeVM : NPCBase
+    {
+        public HomeVM()
+        {
+            Devices = new ObservableCollection<CT100Device>();
+
+            Devices.Add(new CT100Device(){ Name = "aaa" });
+        }
+
+        public ObservableCollection<CT100Device> Devices { get; private set; }
+    }
+}
+
