7b8abf2b9640021b39cc4b2a7a3e13a492583f09
Sinh Pham <phamansinh@gmail.com>
2014-08-07 00:16:54 -0600

    CustomViewRenderers.

---
+++ iOS/Implementators/CustomViewRenderers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using CT100;
+using CT100.iOS;
+
+[assembly: ExportRenderer(typeof(NonSelectableTableView), typeof(NonSelectableTableViewRenderer))]
+namespace CT100.iOS
+{
+    public class NonSelectableTableViewRenderer : TableViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.TableView> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.AllowsSelection = false;
+        }
+    }
+}
+
